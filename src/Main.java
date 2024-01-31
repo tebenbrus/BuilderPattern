b5f35360b04bc05fd8610a8f704305975084288f
Steven Bruce Asis <114350433+tebenbrus@users.noreply.github.com>
2024-01-31 17:09:33 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        User user = new User.UserBuilder("Steven", "Asis", "steven.asis@neu.edu.ph")
+                .address("Quezon City")
+                .phone("09531051374")
+                .age(21)
+                .build();
+
+        System.out.println("User Information");
+        System.out.println("First name: " +user.firstName);
+        System.out.println("Last name: " +user.lastName);
+        System.out.println("Email: " +user.email);
+        System.out.println("Address: " +user.address);
+        System.out.println("Phone: " +user.phone);
+        System.out.println("Age: " +user.age);
+    }
 }
